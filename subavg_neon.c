7b417d2b4a16b300b4c8c748fbfe64781661d541
Luc Trudeau <luc@trud.ca>
2018-02-03 17:03:11 -0500

    Subtracted the average in subavg

--- subavg_neon.c
+++ subavg_neon.c
@@ -1,28 +1,20 @@
 
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 #include <arm_neon.h>
 
+#include "gen_data.h"
 #include "neon_print.h"
 
 #define CFL_BUF_LINE (32)
 
 int main(void) {
+  int32_t round_offset = 8;
+  int32_t numpel_log2 = 4;
+
   int16_t pred_buf[CFL_BUF_LINE * CFL_BUF_LINE] = {0};
-  srand(42);
-  int sum = 0;
-  for (int j = 0; j < 4; j++) {
-    for (int i = 0; i < 4; i++) {
-      int val = (rand() % 1024);
-      pred_buf[j * CFL_BUF_LINE + i] = val;
-      sum += val;
-      printf("%d ", val);
-    }
-    printf("\n");
-  }
-  printf("Sum:%d\n\n", sum);
+  fill_buf(pred_buf, 4, CFL_BUF_LINE);
 
   int16x4_t l0 = vld1_s16(pred_buf);
   print_s16(l0, "Load line 0");
@@ -48,8 +40,22 @@ int main(void) {
   int32x2_t a2 = vadd_s32(hadd0, hadd1);
   print_s32(a2, "Add 2");
 
-  int32_t hadd2 = vaddv_s32(a2);
-  printf("Sum:%d\n\n", hadd2);
+  int32_t sum = vaddv_s32(a2);
+  printf("Sum:%d\n\n", sum);
+
+  int32_t avg = (sum - round_offset) >> numpel_log2;
+  int16x4_t avg_16x4 = vdup_n_s16(avg);
+  printf("Average:%d\n\n", avg);
+
+  int16x4_t s0 = vsub_s16(l0, avg_16x4);
+  int16x4_t s1 = vsub_s16(l1, avg_16x4);
+  int16x4_t s2 = vsub_s16(l2, avg_16x4);
+  int16x4_t s3 = vsub_s16(l3, avg_16x4);
+
+  vst1_s16(pred_buf, s0);
+  vst1_s16(pred_buf + CFL_BUF_LINE, s1);
+  vst1_s16(pred_buf + 2 * CFL_BUF_LINE, s2);
+  vst1_s16(pred_buf + 3 * CFL_BUF_LINE, s3);
 
   return 0;
 }
