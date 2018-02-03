f9699b9589cabe890d1507123a2008456aa34a1d
Luc Trudeau <luc@trud.ca>
2018-02-03 17:50:29 -0500

    Validate neon with C implementation

--- subavg_neon.c
+++ subavg_neon.c
@@ -1,4 +1,5 @@
 
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -9,53 +10,80 @@
 
 #define CFL_BUF_LINE (32)
 
+static void subtract_average_c(int16_t *buf, int width, int height,
+                               int round_offset, int numpel_log2) {
+  int sum = 0;
+  for (int j = 0; j < height; j++) {
+    for (int i = 0; i < width; i++) {
+      sum += buf[j * CFL_BUF_LINE + i];
+    }
+  }
+  const int32_t avg = (sum - round_offset) >> numpel_log2;
+  for (int j = 0; j < height; j++) {
+    for (int i = 0; i < width; i++) {
+      buf[j * CFL_BUF_LINE + i] -= avg;
+    }
+  }
+}
+
+void assert_buf_equals(int16_t *expected, int16_t *actual, int width,
+                       int height, int stride) {
+  for (int j = 0; j < height; j++) {
+    for (int i = 0; i < width; i++) {
+      assert(expected[j * stride + i] == actual[j * stride + i]);
+    }
+  }
+}
+
+static void subtract_average_neon(int16_t *buf, int width, int height,
+                                  int round_offset, int numpel_log2) {
+  const int16_t *const end = buf + height * CFL_BUF_LINE;
+  const int16_t *sum_buf = buf;
+
+  int32_t sum;
+  do {
+    int16x4_t l0 = vld1_s16(sum_buf);
+    int16x4_t l1 = vld1_s16(sum_buf + CFL_BUF_LINE);
+    int16x4_t l2 = vld1_s16(sum_buf + 2 * CFL_BUF_LINE);
+    int16x4_t l3 = vld1_s16(sum_buf + 3 * CFL_BUF_LINE);
+
+    int16x4_t a0 = vadd_s16(l0, l1);
+    int16x4_t a1 = vadd_s16(l2, l3);
+
+    int32x2_t hadd0 = vpaddl_s16(a0);
+    int32x2_t hadd1 = vpaddl_s16(a1);
+
+    int32x2_t a2 = vadd_s32(hadd0, hadd1);
+    sum += vaddv_s32(a2);
+
+    sum_buf += 4 * CFL_BUF_LINE;
+  } while (sum_buf < end);
+
+  const int32_t avg = (sum - round_offset) >> numpel_log2;
+  const int16x4_t avg_16x4 = vdup_n_s16(avg);
+
+  do {
+    vst1_s16(buf, vsub_s16(vld1_s16(buf), avg_16x4));
+
+    buf += CFL_BUF_LINE;
+  } while (buf < end);
+}
+
+void test_subtract_average_neon(int width, int height, int round_offset,
+                                int numpel_log2) {
+  int16_t c_buf[CFL_BUF_LINE * CFL_BUF_LINE] = {0};
+  int16_t neon_buf[CFL_BUF_LINE * CFL_BUF_LINE] = {0};
+
+  fill_buf(c_buf, width, height, CFL_BUF_LINE);
+  fill_buf(neon_buf, width, height, CFL_BUF_LINE);
+
+  subtract_average_c(c_buf, width, height, round_offset, numpel_log2);
+  subtract_average_neon(neon_buf, width, height, round_offset, numpel_log2);
+
+  assert_buf_equals(c_buf, neon_buf, width, height, CFL_BUF_LINE);
+}
+
 int main(void) {
-  int32_t round_offset = 8;
-  int32_t numpel_log2 = 4;
-
-  int16_t pred_buf[CFL_BUF_LINE * CFL_BUF_LINE] = {0};
-  fill_buf(pred_buf, 4, CFL_BUF_LINE);
-
-  int16x4_t l0 = vld1_s16(pred_buf);
-  print_s16(l0, "Load line 0");
-  int16x4_t l1 = vld1_s16(pred_buf + CFL_BUF_LINE);
-  print_s16(l1, "Load line 1");
-
-  int16x4_t a0 = vadd_s16(l0, l1);
-  print_s16(a0, "L0 + L1");
-
-  int16x4_t l2 = vld1_s16(pred_buf + 2 * CFL_BUF_LINE);
-  print_s16(l2, "Load line 2");
-  int16x4_t l3 = vld1_s16(pred_buf + 3 * CFL_BUF_LINE);
-  print_s16(l3, "Load line 3");
-
-  int16x4_t a1 = vadd_s16(l2, l3);
-  print_s16(a1, "L2 + L3");
-
-  int32x2_t hadd0 = vpaddl_s16(a0);
-  print_s32(hadd0, "hadd");
-  int32x2_t hadd1 = vpaddl_s16(a1);
-  print_s32(hadd1, "hadd");
-
-  int32x2_t a2 = vadd_s32(hadd0, hadd1);
-  print_s32(a2, "Add 2");
-
-  int32_t sum = vaddv_s32(a2);
-  printf("Sum:%d\n\n", sum);
-
-  int32_t avg = (sum - round_offset) >> numpel_log2;
-  int16x4_t avg_16x4 = vdup_n_s16(avg);
-  printf("Average:%d\n\n", avg);
-
-  int16x4_t s0 = vsub_s16(l0, avg_16x4);
-  int16x4_t s1 = vsub_s16(l1, avg_16x4);
-  int16x4_t s2 = vsub_s16(l2, avg_16x4);
-  int16x4_t s3 = vsub_s16(l3, avg_16x4);
-
-  vst1_s16(pred_buf, s0);
-  vst1_s16(pred_buf + CFL_BUF_LINE, s1);
-  vst1_s16(pred_buf + 2 * CFL_BUF_LINE, s2);
-  vst1_s16(pred_buf + 3 * CFL_BUF_LINE, s3);
-
-  return 0;
+  test_subtract_average_neon(4, 4, 8, 4);
+  test_subtract_average_neon(4, 8, 16, 5);
 }
