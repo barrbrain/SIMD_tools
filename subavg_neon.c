f76ca5f2aa2c0e25112b45b4eed4de9a88f1be94
Luc Trudeau <luc@trud.ca>
2018-02-03 18:51:27 -0500

    Add Support for 4x16

--- subavg_neon.c
+++ subavg_neon.c
@@ -50,17 +50,13 @@ static void subtract_average_neon(int16_t *buf, int width, int height,
     int16x4_t a0 = vadd_s16(l0, l1);
     int16x4_t a1 = vadd_s16(l2, l3);
 
-    int32x2_t hadd0 = vpaddl_s16(a0);
-    int32x2_t hadd1 = vpaddl_s16(a1);
-
-    int32x2_t a2 = vadd_s32(hadd0, hadd1);
+    int32x2_t a2 = vadd_s32(vpaddl_s16(a0), vpaddl_s16(a1));
     sum += vaddv_s32(a2);
 
     sum_buf += 4 * CFL_BUF_LINE;
   } while (sum_buf < end);
 
-  const int32_t avg = (sum - round_offset) >> numpel_log2;
-  const int16x4_t avg_16x4 = vdup_n_s16(avg);
+  const int16x4_t avg_16x4 = vdup_n_s16((sum - round_offset) >> numpel_log2);
 
   do {
     vst1_s16(buf, vsub_s16(vld1_s16(buf), avg_16x4));
@@ -88,13 +84,30 @@ void test_subtract_average_neon(int width, int height, int round_offset,
     subtract_average_##arch(buf, width, height, round_offset, numpel_log2); \
   }
 
-#define subtract_functions(arch) \
-  subtract_average(arch, 4, 4, 8, 4) subtract_average(arch, 4, 8, 16, 5)
+#define subtract_functions(arch)                                         \
+  subtract_average(arch, 4, 4, 8, 4) subtract_average(arch, 4, 8, 16, 5) \
+      subtract_average(arch, 4, 16, 32, 6)
+
+#define test_subtract_average(width, height)                         \
+  void test_subtract_average_##width##x##height() {                  \
+    int16_t c_buf[CFL_BUF_LINE * CFL_BUF_LINE] = {0};                \
+    int16_t neon_buf[CFL_BUF_LINE * CFL_BUF_LINE] = {0};             \
+    fill_buf(c_buf, width, height, CFL_BUF_LINE);                    \
+    fill_buf(neon_buf, width, height, CFL_BUF_LINE);                 \
+    subtract_average_##width##x##height##_c(c_buf);                  \
+    subtract_average_##width##x##height##_neon(neon_buf);            \
+    assert_buf_equals(c_buf, neon_buf, width, height, CFL_BUF_LINE); \
+  }
 
 subtract_functions(c);
 subtract_functions(neon);
 
+test_subtract_average(4, 4);
+test_subtract_average(4, 8);
+test_subtract_average(4, 16);
+
 int main(void) {
-  test_subtract_average_neon(4, 4, 8, 4);
-  test_subtract_average_neon(4, 8, 16, 5);
+  test_subtract_average_4x4();
+  test_subtract_average_4x8();
+  test_subtract_average_4x16();
 }
