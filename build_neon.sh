12d12c382618f71df7dcb57fe58d1a97e8a9ec9c
Luc Trudeau <luc@trud.ca>
2018-02-03 16:58:05 -0500

    Change compiler to use aarch64
    Fixes issue #1

--- build_neon.sh
+++ build_neon.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-COMPILER=arm-linux-gnueabi-gcc
-FLAGS="-march=armv8-a -mfpu=neon-fp-armv8 -mfloat-abi=softfp -O3"
+COMPILER=aarch64-linux-gnu-gcc
+FLAGS="-march=armv8-a -O3"
 
 $COMPILER -S $FLAGS $1.c -o $1.s
 $COMPILER $FLAGS $1.c -o $1
 
-qemu-aarch64 -L /usr/arm-linux-gnueabi/ ./$1
+qemu-aarch64 -L /usr/aarch64-linux-gnu/ ./$1
+ 
